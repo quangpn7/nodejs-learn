543eab6a86e3a8e4cc74b1b0bab3c5d6aa1e882f
quangpn7 <nhutquang.mca@gmail.com>
2024-03-30 12:32:55 +0700

    Init

---
+++ dist/index.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const dotenv = require("dotenv");
+dotenv.config();
+const app = express();
+const port = process.env.PORT;
+console.log({ port });
+app.get("/", (req, res) => {
+    res.send("Express + Typsript gegege Server");
+});
+app.listen(port, () => {
+    console.log(`[server]: Server is running at http://localhost:${port}`);
+});
